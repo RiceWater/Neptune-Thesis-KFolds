2a7922b279f71802172597734f7cbd165e0d61c7
RiceWater <chuawillian11@gmail.com>
2024-05-14 16:18:17 +0800

    Made Filter Notebook
    updated notebooks too for vizualization and stuff

--- neptune-download-file.py
+++ neptune-download-file.py
@@ -4,8 +4,8 @@ import neptune
 from neptune.integrations.tensorflow_keras import NeptuneCallback
 
 # Information needed to download files
-project_name = 'New-Thesis/Basic'
-run_id = 'BAS-2'
+project_name = 'New-Thesis/KFold-Test'
+run_id = 'KFTEST-58'
 
 # Load specific run that has the model and set mode to read-only
 run = neptune.init_run(project=project_name,
@@ -13,9 +13,10 @@ run = neptune.init_run(project=project_name,
                        mode="read-only")
 
 # Download keras file from '/checkpoint/my_model' to current working directory
-run['/checkpoint/my_model'].download()
+run['/checkpoint/model'].download()
 
 # Download the file to a specific directory
 # run["data/sample"].download(destination="path/to/destination")
 
 run.stop()
+
